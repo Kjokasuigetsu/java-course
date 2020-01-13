788ed5b0c7b8a6602a104528870792f2b27f9009
Kjokasuigetsu <lexsandrbk@gmail.com>
2020-01-14 01:15:49 +0300

    update Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,15 @@
 public class Calculate {
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		int chastnoe = first - second;
+		double umn = first * second;
+		double del = first / second;
+		System.out.println("Sum: " + sum);
+		System.out.println("Chastnoe: " + chastnoe);
+		System.out.println("Umnojenie: " + umn);
+		System.out.println("Delenie: " + del);
 	}
 }
